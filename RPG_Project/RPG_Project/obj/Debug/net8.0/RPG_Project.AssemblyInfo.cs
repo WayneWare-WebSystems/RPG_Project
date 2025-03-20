47ba358d78e04aa55070b84af4566d9d7dc6c730
Wayne Ware WebSystems <wayneware.websystems@gmail.com>
2025-03-20 20:12:37 -0300

    Ajustes
    .

--- RPG_Project/RPG_Project/obj/Debug/net8.0/RPG_Project.AssemblyInfo.cs
+++ RPG_Project/RPG_Project/obj/Debug/net8.0/RPG_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d99a2afe8799fe9ed7a72401b856090bad096f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4f6f5c774b986235945669b8bee8bfb7124083")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
