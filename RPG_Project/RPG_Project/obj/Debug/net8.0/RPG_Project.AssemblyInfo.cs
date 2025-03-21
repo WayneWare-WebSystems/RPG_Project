9fda913e4b8c57d212e5d5977cd0f698c7532f0c
IgorBalsante <igornunesbalsante@gmail.com>
2025-03-21 20:12:25 -0300

    alteração do front - home
    .

--- RPG_Project/RPG_Project/obj/Debug/net8.0/RPG_Project.AssemblyInfo.cs
+++ RPG_Project/RPG_Project/obj/Debug/net8.0/RPG_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4f6f5c774b986235945669b8bee8bfb7124083")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a637941dfc1c2ee9ad4b9c602621405df73b5e06")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
